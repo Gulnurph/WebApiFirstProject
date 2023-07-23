989601e34f3c393d61a467c0959aaf700c3a7ea4
Gülnur <84268834+Gulnurph@users.noreply.github.com>
2023-07-23 22:55:04 +0300

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static readonly string[] products =
+        {
+            "Samsun a1","galaxy s3"
+        };
+        [HttpGet]
+        public string[] GetProducts()
+        {
+            return products;
+        }
+        [HttpGet("{id}")]//iki GET METTOT AYIRT ETMEK İÇİN ID BİLGİSİNİ VER
+        public string GetProduct(int id)
+        {
+            return products[id];
+        }
+    }
+}
